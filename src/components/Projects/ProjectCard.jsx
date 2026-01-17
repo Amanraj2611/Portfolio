698843870225769d2d2b0eb6c16cb8369b57f188
Aman <rajaman2611@gmail.com>
2026-01-17 22:49:43 +0530

    Updated portfolio UI

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
-import projectImage from "../../assets/projects/projects.png";
+
 import bankImg from "../../assets/projects/bank.jpg";
 import votingImg from "../../assets/projects/voting.jpg";
 import electricityImg from "../../assets/projects/electricity.jpg";
 import amazonImg from "../../assets/projects/amazon.jpg";
 import portfolioImg from "../../assets/projects/portfolio.png";
+import UserManagement from "../../assets/projects/UserManagement.png";
+
 const imageMap = {
- // "projects/project.png": projectImage,
   "projects/bank.jpg": bankImg,
   "projects/voting.jpg": votingImg,
   "projects/electricity.jpg": electricityImg,
   "projects/amazon.jpg": amazonImg,
   "projects/portfolio.png": portfolioImg,
+  "projects/UserManagement.png": UserManagement,
 };
 
 export const ProjectCard = ({
@@ -20,23 +22,52 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
+      
+      {/* Project Image */}
       <img
-        src={imageMap[imageSrc]} 
-        alt={`Image of ${title}`}
+        src={imageMap[imageSrc]}
+        alt={title}
         className={styles.image}
       />
+
+      {/* Title */}
       <h3 className={styles.title}>{title}</h3>
+
+      {/* Description */}
       <p className={styles.description}>{description}</p>
+
+      {/* Skills */}
       <ul className={styles.skills}>
-        {skills.map((skill, id) => (
-          <li key={id} className={styles.skill}>
+        {skills.map((skill, index) => (
+          <li key={index} className={styles.skill}>
             {skill}
           </li>
         ))}
       </ul>
+
+      {/* Links */}
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>Demo</a>
-        <a href={source} className={styles.link}>Source</a>
+        {demo && (
+          <a
+            href={demo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.link} ${styles.demo}`}
+          >
+            Demo
+          </a>
+        )}
+
+        {source && (
+          <a
+            href={source}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.link} ${styles.source}`}
+          >
+            Source
+          </a>
+        )}
       </div>
     </div>
   );
